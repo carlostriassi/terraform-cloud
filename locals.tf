fa503c83ac482ddd145196a3d8c32497f4a0cc0a
Carlos Triassi <ctriassi@hitachisolutions.com>
2023-08-03 14:53:36 -0700

    update locals

--- locals.tf
+++ locals.tf
@@ -1,8 +1,7 @@
 locals {
-  resource_group_name="app-grp"
-  location="East US"
-  virtual_network={
-    name="app-network"
-    address_space="10.0.0.0/16"
+  location="East US"  
+  address_space={
+    Staging="10.0.0.0/16"
+    Test="10.1.0.0/16"
   }
 }
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_resource_group" "resourcegrp" {
-  name     =local.resource_group_name
+  name     ="${terraform.workspace}-grp"
   location = local.location  
 }
 
 resource "azurerm_virtual_network" "network" {
-  name                = local.virtual_network.name
+  name                = "${terraform.workspace}-network"
   location            = local.location  
-  resource_group_name = local.resource_group_name
-  address_space       = [local.virtual_network.address_space]
+  resource_group_name = azurerm_resource_group.resourcegrp.name
+  address_space       = [local.address_space[terraform.workspace]]
   depends_on = [
     azurerm_resource_group.resourcegrp
   ]  
@@ -15,10 +15,10 @@ resource "azurerm_virtual_network" "network" {
 
 
 resource "azurerm_subnet" "subnet" {    
-    name                 = "SubnetA"
-    resource_group_name  = local.resource_group_name
-    virtual_network_name = local.virtual_network.name
-    address_prefixes     = [cidrsubnet(local.virtual_network.address_space,8,0)]
+    name                 = "${terraform.workspace}subnet"
+    resource_group_name  = azurerm_resource_group.resourcegrp.name
+    virtual_network_name = azurerm_virtual_network.network.name
+    address_prefixes     = [cidrsubnet(local.address_space[terraform.workspace],8,0)]
     depends_on = [
       azurerm_virtual_network.network
     ]
